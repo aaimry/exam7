0fef673360c410da42f34f3f2e89d13f822ec6e1
Aaimry <wheal28@gmail.com>
2022-02-05 18:14:42 +0600

    Зарегистрировала модель в админке

--- source/questionnaire/admin.py
+++ source/questionnaire/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Poll, Choice, Answer
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['question', 'create_date']
+    list_filter = ['question']
+    search_fields = ['question']
+    fields = ['question']
+
+
+admin.site.register(Poll, PollAdmin)
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_text', 'poll']
+    list_filter = ['choice_text']
+    search_fields = ['choice_text']
+    fields = ['choice_text', 'poll']
+
+
+admin.site.register(Choice, ChoiceAdmin)
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['poll', 'choice', 'datetime']
+    list_filter = ['poll']
+    search_fields = ['poll']
+    fields = ['poll', 'choice']
+
+    
+admin.site.register(Answer, AnswerAdmin)
