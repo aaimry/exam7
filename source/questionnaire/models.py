89ae5d9e59de4ce7990d550c5cb0956d776b59f1
Aaimry <wheal28@gmail.com>
2022-02-05 18:51:02 +0600

    Создан absolute_url

--- source/questionnaire/models.py
+++ source/questionnaire/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Poll(models.Model):
     question = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Вопрос')
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def get_absolute_url(self):
+        return reverse('poll_check', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.question
@@ -18,6 +22,9 @@ class Choice(models.Model):
     choice_text = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Вариант')
     poll = models.ForeignKey('Poll', on_delete=models.CASCADE, related_name='poll',
                              verbose_name='Опрос')
+
+    def get_absolute_url(self):
+        return reverse('poll_check', kwargs={'pk': self.poll.pk})
 
     def __str__(self):
         return f'{self.choice_text} : {self.poll}'
