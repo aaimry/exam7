13a6358d427af67a393493e94510280de1c723e6
Aaimry <wheal28@gmail.com>
2022-02-05 18:13:41 +0600

    созданы модели Poll, Choice, Answer

--- source/questionnaire/models.py
+++ source/questionnaire/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Вопрос')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        db_table = 'Poll'
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+class Choice(models.Model):
+    choice_text = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Вариант')
+    poll = models.ForeignKey('Poll', on_delete=models.CASCADE, related_name='poll',
+                             verbose_name='Опрос')
+
+    def __str__(self):
+        return f'{self.choice_text} : {self.poll}'
+
+    class Meta:
+        db_table = 'Choice'
+        verbose_name = 'Вариант'
+        verbose_name_plural = 'Варианты'
+
+
+class Answer(models.Model):
+    poll = models.ForeignKey('Poll', on_delete=models.CASCADE, related_name='poll_answer',
+                             verbose_name='Опрос')
+    datetime = models.DateTimeField(auto_now_add=True, verbose_name='Дата заполнения')
+    choice = models.ForeignKey('Choice', on_delete=models.CASCADE, related_name='choice_answer', verbose_name='Вариант')
+
+    def __str__(self):
+        return f'{self.poll} : {self.choice}'
+
+    class Meta:
+        db_table = 'Answer'
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
